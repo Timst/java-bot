8f5758f90f8a0ffe7f2ae6aba83ef00ae4def974
ThomasGirard <arrg.ch@gmail.com>
2015-08-13 11:40:14 +0200

    Split BotImpl into an IRC part and a Logic part, which manages the db and handlers

--- src/main/java/ch/arrg/javabot/BotImpl.java
+++ src/main/java/ch/arrg/javabot/BotImpl.java
@@ -1,26 +1,12 @@
 package ch.arrg.javabot;
 
 import java.io.IOException;
-import java.util.Map;
-import java.util.StringJoiner;
-import java.util.TreeMap;
 
 import org.jibble.pircbot.IrcException;
 import org.jibble.pircbot.NickAlreadyInUseException;
 import org.jibble.pircbot.PircBot;
 
-import ch.arrg.javabot.data.DataStoreUtils;
 import ch.arrg.javabot.data.UserData;
-import ch.arrg.javabot.data.UserDb;
-import ch.arrg.javabot.handlers.HelloHandler;
-import ch.arrg.javabot.handlers.QuestionHandler;
-import ch.arrg.javabot.handlers.QuitHandler;
-import ch.arrg.javabot.handlers.RecordHandler;
-import ch.arrg.javabot.handlers.TimeHandler;
-import ch.arrg.javabot.handlers.UserInfoHandler;
-import ch.arrg.javabot.handlers.quiz.GuessWhoHandler;
-import ch.arrg.javabot.handlers.quiz.GuessWordHandler;
-import ch.arrg.javabot.util.HandlerUtils;
 
 /**
  * Main bot logic
@@ -29,34 +15,16 @@ import ch.arrg.javabot.util.HandlerUtils;
  */
 public class BotImpl extends PircBot implements Bot {
 
-	private Map<String, CommandHandler> handlers = new TreeMap<>();
-
-	private final UserDb userDb;
+	private BotLogic logic = new BotLogic();
 
 	public BotImpl() throws Exception {
-		addHandler(new HelloHandler());
-		addHandler(new TimeHandler());
-		addHandler(new RecordHandler());
-		addHandler(new UserInfoHandler());
-		addHandler(new QuestionHandler());
-		addHandler(new GuessWhoHandler());
-		addHandler(new GuessWordHandler());
-		addHandler(new QuitHandler());
-
 		setName(Const.BOT_NAME);
 		setLogin(Const.BOT_NAME);
-
-		userDb = DataStoreUtils.fromFile(Const.DATA_FILE);
-		DataStoreUtils.saveOnQuit(Const.DATA_FILE, userDb);
+		setEncoding("utf-8");
 	}
 
 	public void start() throws NickAlreadyInUseException, IOException, IrcException {
-		setEncoding("utf-8");
 		connect(Const.SERVER_URL, Const.SERVER_PORT);
-	}
-
-	private void addHandler(CommandHandler h) {
-		handlers.put(h.getName(), h);
 	}
 
 	@Override
@@ -67,43 +35,12 @@ public class BotImpl extends PircBot implements Bot {
 
 	@Override
 	protected void onMessage(String channel, String sender, String login, String hostname, String message) {
-
-		message = message.trim();
-
-		if (message.startsWith("+help")) {
-			onHelp(channel, message);
-			return;
-		}
-
-		for (CommandHandler handler : handlers.values()) {
-			try {
-				handler.handle(this, channel, sender, login, hostname, message);
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
+		logic.onMessage(this, channel, sender, login, hostname, message);
 	}
 
 	@Override
 	protected void onJoin(String channel, String sender, String login, String hostname) {
 
-	}
-
-	private void onHelp(String channel, String message) {
-
-		String handlerName = HandlerUtils.getWord(message, 1);
-		if (handlers.containsKey(handlerName)) {
-			handlers.get(handlerName).help(HandlerUtils.makeReplyer(this, channel), message);
-		} else {
-
-			sendMsg(channel, "Here are known handlers: ");
-			StringJoiner join = new StringJoiner(" ");
-			for (CommandHandler handler : handlers.values()) {
-				join.add(handler.getName());
-			}
-			sendMsg(channel, join.toString());
-			sendMsg(channel, "Send +help <handler> to get more info on a particular one.");
-		}
 	}
 
 	@Override
@@ -113,7 +50,7 @@ public class BotImpl extends PircBot implements Bot {
 
 	@Override
 	public UserData getUserData(String user) {
-		return userDb.getOrCreateUserData(user);
+		return logic.getUserData(user);
 	}
 
 	@Override
---
+++ src/main/java/ch/arrg/javabot/BotLogic.java
@@ -0,0 +1,86 @@
+package ch.arrg.javabot;
+
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.TreeMap;
+
+import ch.arrg.javabot.data.DataStoreUtils;
+import ch.arrg.javabot.data.UserData;
+import ch.arrg.javabot.data.UserDb;
+import ch.arrg.javabot.handlers.HelloHandler;
+import ch.arrg.javabot.handlers.QuestionHandler;
+import ch.arrg.javabot.handlers.QuitHandler;
+import ch.arrg.javabot.handlers.RecordHandler;
+import ch.arrg.javabot.handlers.TimeHandler;
+import ch.arrg.javabot.handlers.UserInfoHandler;
+import ch.arrg.javabot.handlers.quiz.GuessWhoHandler;
+import ch.arrg.javabot.handlers.quiz.GuessWordHandler;
+import ch.arrg.javabot.util.HandlerUtils;
+import ch.arrg.javabot.util.Replyer;
+
+public class BotLogic {
+
+	private Map<String, CommandHandler> handlers = new TreeMap<>();
+
+	private final UserDb userDb;
+
+	public BotLogic() throws Exception {
+		addHandler(new HelloHandler());
+		addHandler(new TimeHandler());
+		addHandler(new RecordHandler());
+		addHandler(new UserInfoHandler());
+		addHandler(new QuestionHandler());
+		addHandler(new GuessWhoHandler());
+		addHandler(new GuessWordHandler());
+		addHandler(new QuitHandler());
+
+		userDb = DataStoreUtils.fromFile(Const.DATA_FILE);
+		DataStoreUtils.saveOnQuit(Const.DATA_FILE, userDb);
+	}
+
+	private void addHandler(CommandHandler h) {
+		handlers.put(h.getName(), h);
+	}
+
+	protected void onMessage(Bot bot, String channel, String sender, String login, String hostname, String message) {
+
+		Replyer rep = HandlerUtils.makeReplyer(bot, channel);
+
+		message = message.trim();
+
+		if (message.startsWith("+help")) {
+			onHelp(rep, message);
+			return;
+		}
+
+		for (CommandHandler handler : handlers.values()) {
+			try {
+				handler.handle(bot, channel, sender, login, hostname, message);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	private void onHelp(Replyer rep, String message) {
+
+		String handlerName = HandlerUtils.getWord(message, 1);
+		if (handlers.containsKey(handlerName)) {
+			handlers.get(handlerName).help(rep, message);
+		} else {
+
+			rep.send("Here are known handlers: ");
+			StringJoiner join = new StringJoiner(" ");
+			for (CommandHandler handler : handlers.values()) {
+				join.add(handler.getName());
+			}
+			rep.send(join.toString());
+			rep.send("Send +help <handler> to get more info on a particular one.");
+		}
+	}
+
+	public UserData getUserData(String user) {
+		return userDb.getOrCreateUserData(user);
+	}
+
+}
