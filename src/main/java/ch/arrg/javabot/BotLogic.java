ab3264f311c96822279b37b767a464357c5821f4
ThomasGirard <arrg.ch@gmail.com>
2015-11-30 22:39:13 +0000

    Make project java7 compatible (remove joiners)

--- src/main/java/ch/arrg/javabot/BotLogic.java
+++ src/main/java/ch/arrg/javabot/BotLogic.java
@@ -1,7 +1,6 @@
 package ch.arrg.javabot;
 
 import java.util.Map;
-import java.util.StringJoiner;
 import java.util.TreeMap;
 
 import ch.arrg.javabot.data.BotContext;
@@ -18,11 +17,11 @@ import ch.arrg.javabot.handlers.quiz.GuessWordHandler;
 import ch.arrg.javabot.util.CommandMatcher;
 
 public class BotLogic {
-
+	
 	private Map<String, CommandHandler> handlers = new TreeMap<>();
-
+	
 	private final UserDb userDb;
-
+	
 	public BotLogic() throws Exception {
 		addHandler(new HelloHandler());
 		addHandler(new TimeHandler());
@@ -32,25 +31,25 @@ public class BotLogic {
 		addHandler(new GuessWhoHandler());
 		addHandler(new GuessWordHandler());
 		addHandler(new AdminHandler());
-
+		
 		userDb = DataStoreUtils.fromFile(Const.DATA_FILE);
 		DataStoreUtils.saveOnQuit(Const.DATA_FILE, userDb);
 	}
-
+	
 	private void addHandler(CommandHandler h) {
 		handlers.put(h.getName(), h);
 	}
-
+	
 	protected void onMessage(BotContext ctx) {
 		String message = ctx.message.trim();
-
+		
 		CommandMatcher matcher = CommandMatcher.make("+help");
-		if (matcher.matches(message)) {
+		if(matcher.matches(message)) {
 			onHelp(ctx, matcher.nextWord());
 			return;
 		}
-
-		for (CommandHandler handler : handlers.values()) {
+		
+		for(CommandHandler handler : handlers.values()) {
 			try {
 				handler.handle(ctx);
 			} catch (Exception e) {
@@ -58,25 +57,25 @@ public class BotLogic {
 			}
 		}
 	}
-
+	
 	private void onHelp(BotContext ctx, String topic) {
-
-		if (handlers.containsKey(topic)) {
+		
+		if(handlers.containsKey(topic)) {
 			handlers.get(topic).help(ctx);
 		} else {
-
+			
 			ctx.reply("Here are known handlers: ");
-			StringJoiner join = new StringJoiner(" ");
-			for (CommandHandler handler : handlers.values()) {
-				join.add(handler.getName());
+			StringBuilder sb = new StringBuilder();
+			for(CommandHandler handler : handlers.values()) {
+				sb.append(handler.getName()).append(" ");
 			}
-			ctx.reply(join.toString());
+			ctx.reply(sb.toString());
 			ctx.reply("Send +help <handler> to get more info on a particular one.");
 		}
 	}
-
+	
 	public UserData getUserData(String user) {
 		return userDb.getOrCreateUserData(user);
 	}
-
+	
 }
--- src/main/java/ch/arrg/javabot/handlers/RecordHandler.java
+++ src/main/java/ch/arrg/javabot/handlers/RecordHandler.java
@@ -7,42 +7,44 @@ import ch.arrg.javabot.CommandHandler;
 import ch.arrg.javabot.data.BotContext;
 import ch.arrg.javabot.util.CommandMatcher;
 
+import com.google.common.base.Joiner;
+
 public class RecordHandler implements CommandHandler {
-
+	
 	private final List<String> allowedRecords = new ArrayList<>();
-
+	
 	public RecordHandler() {
 		allowedRecords.add(TimeHandler.TZ_RECORD);
 	}
-
+	
 	@Override
 	public void handle(BotContext ctx) {
-
+		
 		CommandMatcher matcher = CommandMatcher.make("+record");
-		if (matcher.matches(ctx.message)) {
+		if(matcher.matches(ctx.message)) {
 			String action = matcher.nextWord();
-
-			if (allowedRecords.contains(action)) {
+			
+			if(allowedRecords.contains(action)) {
 				String value = matcher.nextWord();
 				ctx.setRecord(action, value);
 				ctx.reply("Okay I will remember.");
 			} else {
 				ctx.reply("This is now a known key, doot");
 			}
-
+			
 		}
 	}
-
+	
 	@Override
 	public String getName() {
 		return "+record";
 	}
-
+	
 	@Override
 	public void help(BotContext ctx) {
 		ctx.reply("Set info about yourself.");
 		ctx.reply("Use +record <key> <value> to set a record.");
-		String allowed = String.join(", ", allowedRecords);
+		String allowed = Joiner.on(", ").join(allowedRecords);
 		ctx.reply("Allowed records are: [" + allowed + "]");
 	}
 }
