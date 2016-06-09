de54da22c5c54120979f713f150c504623140f17
ThomasGirard <arrg.ch@gmail.com>
2016-06-09 23:10:43 +0100

    Fix logging

--- src/main/java/ch/arrg/javabot/log/DatabaseLogService.java
+++ src/main/java/ch/arrg/javabot/log/DatabaseLogService.java
@@ -1,14 +1,15 @@
 package ch.arrg.javabot.log;
 
 import java.sql.Connection;
-import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Timestamp;
 
 import ch.arrg.javabot.Const;
 import ch.arrg.javabot.data.BotContext;
 
+// TODO Database logging as a handler ?
 public class DatabaseLogService {
 	
 	public enum LogEvent {
@@ -29,37 +30,37 @@ public class DatabaseLogService {
 	
 	public static void logEvent(LogEvent inType, BotContext ctx) {
 		try (Connection conn = getConnection()) {
-			
-			String query = "INSERT INTO `?` (channel, name, time, message, type, hidden) VALUES (?, ?, ?, ?, ?, ?)";
+			String query = "INSERT INTO `"
+					+ Const.DB_TABLE
+					+ "` (channel, name, time, message, type, hidden) VALUES (?, ?, ?, ?, ?, ?)";
 			PreparedStatement preparedStatement = conn.prepareStatement(query);
 			
 			String channel = ctx.channel;
 			String name = ctx.sender;
-			Date time = new Date(new java.util.Date().getTime());
+			Timestamp time = new Timestamp(new java.util.Date().getTime());
 			String message = ctx.message;
 			String type = inType.dbName;
 			String hidden = "F";
 			
-			// Parameters start with 1
-			preparedStatement.setString(1, Const.DB_TABLE);
-			preparedStatement.setString(2, channel);
-			preparedStatement.setString(3, name);
-			preparedStatement.setDate(4, time);
-			preparedStatement.setString(5, message);
-			preparedStatement.setString(6, type);
-			preparedStatement.setString(7, hidden);
+			preparedStatement.setString(1, channel);
+			preparedStatement.setString(2, name);
+			preparedStatement.setTimestamp(3, time);
+			preparedStatement.setString(4, message);
+			preparedStatement.setString(5, type);
+			preparedStatement.setString(6, hidden);
+			
 			preparedStatement.executeUpdate();
 			
 		} catch (ClassNotFoundException | SQLException e) {
-			// TODO when logging to DB fails, dump to file ?
-			throw new IllegalStateException("Couldn't log.", e);
+			e.printStackTrace();
 		}
 	}
 	
 	private static Connection getConnection() throws ClassNotFoundException, SQLException {
-		// This will load the MySQL driver, each DB has its own driver
+		// Load DB driver
 		Class.forName(Const.DB_DRIVER);
-		// Setup the connection with the DB
+		
+		// Connect using db string
 		Connection connect = DriverManager.getConnection(Const.DB_CONN_STRING);
 		return connect;
 	}
