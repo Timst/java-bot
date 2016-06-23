c677bdd2c5709eb20677f6b827e7dd3377aec345
ThomasGirard <arrg.ch@gmail.com>
2016-06-23 22:47:51 +0100

    LastSeenHandler

---
+++ src/main/java/ch/arrg/javabot/handlers/LastSeenHandler.java
@@ -0,0 +1,52 @@
+package ch.arrg.javabot.handlers;
+
+import java.util.Date;
+
+import ch.arrg.javabot.CommandHandler;
+import ch.arrg.javabot.data.BotContext;
+import ch.arrg.javabot.log.DatabaseLogService;
+import ch.arrg.javabot.log.LogLine;
+import ch.arrg.javabot.util.CommandMatcher;
+import ch.arrg.javabot.util.HandlerUtils;
+
+public class LastSeenHandler implements CommandHandler {
+	
+	@Override
+	public void handle(BotContext ctx) {
+		
+		CommandMatcher matcher = CommandMatcher.make("+lastseen");
+		if(matcher.matches(ctx.message)) {
+			String user = matcher.nextWord();
+			
+			findLastSeen(ctx, user);
+		}
+	}
+	
+	private void findLastSeen(BotContext ctx, String user) {
+		Date lastSeen = getLastSeenTime(ctx.channel, user);
+		if(lastSeen == null) {
+			ctx.reply("I never heard of that \"nujabes\" you're asking about ?");
+		} else {
+			ctx.reply(user + " was last seen " + HandlerUtils.prettyDate(lastSeen));
+		}
+	}
+	
+	private Date getLastSeenTime(String channel, String user) {
+		LogLine msg = DatabaseLogService.lastMessageByUser(channel, user);
+		if(msg != null) {
+			return msg.date;
+		}
+		
+		return null;
+	}
+	
+	@Override
+	public String getName() {
+		return "+lastseen";
+	}
+	
+	@Override
+	public void help(BotContext ctx) {
+		ctx.reply("Ask when a user was last chatting: +lastseen <user>");
+	}
+}
