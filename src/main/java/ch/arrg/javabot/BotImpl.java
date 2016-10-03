86a74a54aaa50b182a1c05b2faaece9f468686e3
ThomasGirard <arrg.ch@gmail.com>
2016-10-03 23:28:25 +0100

    Make bot answer to private messages

--- src/main/java/ch/arrg/javabot/BotImpl.java
+++ src/main/java/ch/arrg/javabot/BotImpl.java
@@ -18,25 +18,25 @@ import ch.arrg.javabot.log.LogEvent;
  *
  * @author tgi */
 public class BotImpl extends PircBot implements Bot {
-	
+
 	private static final String ENCODING = "utf-8";
-	
+
 	private BotLogic logic = new BotLogic();
-	
+
 	private boolean isPaused = false;
-	
+
 	private List<String> ignoredUsers = Lists.newArrayList(Const.strArray("bot.ignoredUsers"));
-	
+
 	public BotImpl() throws Exception {
 		setName(Const.BOT_NAME);
 		setLogin(Const.BOT_NAME);
 		setEncoding(ENCODING);
 	}
-	
+
 	public void start() throws NickAlreadyInUseException, IOException, IrcException {
 		connect(Const.SERVER_URL, Const.SERVER_PORT);
 	}
-	
+
 	@Override
 	protected void onConnect() {
 		super.onConnect();
@@ -45,90 +45,106 @@ public class BotImpl extends PircBot implements Bot {
 			joinChannel(chan);
 		}
 	}
-	
+
 	@Override
-	protected void onMessage(String channel, String sender, String login, String hostname, String message) {
+	protected void onPrivateMessage(String sender, String login, String hostname, String message) {
+		// TODO hack : this will make all replies go the private conv, but it'd
+		// be better if the context was privmsg aware.
+		String channel = sender;
 		BotContext ctx = new BotContext(this, channel, sender, login, hostname, message);
-		
-		DatabaseLogServiceProvider.get().logEvent(LogEvent.MESSAGE, ctx);
+
+		// DatabaseLogServiceProvider.get().logEvent(LogEvent.MESSAGE, ctx);
 		
 		if(ignoredUsers.contains(sender)) {
 			return;
 		}
-		
+
 		logic.onMessage(ctx);
 	}
-	
+
+	@Override
+	protected void onMessage(String channel, String sender, String login, String hostname, String message) {
+		BotContext ctx = new BotContext(this, channel, sender, login, hostname, message);
+
+		DatabaseLogServiceProvider.get().logEvent(LogEvent.MESSAGE, ctx);
+
+		if(ignoredUsers.contains(sender)) {
+			return;
+		}
+
+		logic.onMessage(ctx);
+	}
+
 	@Override
 	protected void onJoin(String channel, String sender, String login, String hostname) {
 		BotContext ctx = new BotContext(this, channel, sender, login, hostname, null);
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.JOIN, ctx);
 		logic.onJoin(ctx);
 	}
-	
+
 	@Override
 	protected void onQuit(String sender, String login, String hostname, String reason) {
 		BotContext ctx = new BotContext(this, Const.CHANNEL, sender, login, hostname, reason);
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.QUIT, ctx);
 	}
-	
+
 	@Override
 	protected void onPart(String channel, String sender, String login, String hostname) {
 		BotContext ctx = new BotContext(this, channel, sender, login, hostname, "part");
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.PART, ctx);
 	}
-	
+
 	@Override
 	protected void onAction(String sender, String login, String hostname, String channel, String action) {
 		BotContext ctx = new BotContext(this, channel, sender, login, hostname, action);
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.JOIN, ctx);
 	}
-	
+
 	@Override
 	protected void onTopic(String channel, String topic, String setBy, long date, boolean changed) {
 		// TODO logging topic : hostname and co ?
 		BotContext ctx = new BotContext(this, channel, setBy, setBy, setBy, topic);
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.TOPIC, ctx);
 	}
-	
+
 	@Override
 	protected void onNickChange(String oldNick, String login, String hostname, String newNick) {
 		BotContext ctx = new BotContext(this, Const.CHANNEL, oldNick, login, hostname, newNick);
-		
+
 		DatabaseLogServiceProvider.get().logEvent(LogEvent.NICK, ctx);
 	}
-	
+
 	@Override
 	public void sendMsg(String target, String message) {
 		sendMessage(target, message);
 	}
-	
+
 	@Override
 	public UserData getUserData(String user) {
 		return logic.getUserData(user);
 	}
-	
+
 	@Override
 	public void quit() {
 		quitServer(Const.QUIT_MESSAGE);
 		System.exit(1);
 	}
-	
+
 	@Override
 	public void adminPause() {
 		isPaused = true;
 	}
-	
+
 	@Override
 	public void adminUnpause() {
 		isPaused = false;
 	}
-	
+
 	@Override
 	public boolean isPaused() {
 		return isPaused;
 	}
-	
+
 	@Override
 	public Boolean toggleHandler(String handlerName) {
 		return logic.toggleHandler(handlerName);
