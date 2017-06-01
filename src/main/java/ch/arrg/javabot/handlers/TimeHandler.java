2f5a15fe0d425d524ad659db471d6ec5e7d97ce8
ThomasGirard <arrg.ch@gmail.com>
2017-06-01 20:54:19 +0100

    Using +time with an unknown timezone gives an error instead of silently using GMT

--- src/main/java/ch/arrg/javabot/handlers/TimeHandler.java
+++ src/main/java/ch/arrg/javabot/handlers/TimeHandler.java
@@ -9,6 +9,8 @@ import java.util.TimeZone;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.ArrayUtils;
+
 import com.google.common.base.Joiner;
 
 import ch.arrg.javabot.CommandHandler;
@@ -18,19 +20,19 @@ import ch.arrg.javabot.util.CommandMatcher;
 import ch.arrg.javabot.util.Logging;
 
 public class TimeHandler implements CommandHandler {
-	
-	public static final String TZ_RECORD = "tz";
 
+	public static final String TZ_RECORD = "tz";
+	
 	// TODO make that a runtime setting
 	private final static boolean IMPLICIT_MODE_ENABLED = false;
-
+	
 	private final static String TIME_REGEX_IMPLICIT = "(\\d{1,2}) ?: ?(\\d{2})\\b";
 	private final static String TIME_REGEX_EXPLICIT = "\\+(\\d{1,2}) ?[h:] ?(\\d{2})\\b";
-	
-	private final Pattern timePattern;
 
-	private final static String[] outputZones = Const.strArray("TimeHandler.targetZones");
+	private final Pattern timePattern;
 	
+	private final static String[] outputZones = Const.strArray("TimeHandler.targetZones");
+
 	public TimeHandler() {
 		if(IMPLICIT_MODE_ENABLED) {
 			timePattern = Pattern.compile(TIME_REGEX_IMPLICIT);
@@ -38,11 +40,11 @@ public class TimeHandler implements CommandHandler {
 			timePattern = Pattern.compile(TIME_REGEX_EXPLICIT);
 		}
 	}
-
+	
 	@Override
 	public void handle(BotContext ctx) {
 		String m = ctx.message;
-		
+
 		CommandMatcher matcher = CommandMatcher.make("+time");
 		if(matcher.matches(m)) {
 			handleTimeCommand(ctx, matcher);
@@ -53,12 +55,12 @@ public class TimeHandler implements CommandHandler {
 				handleTimeRequest(ctx, tr);
 			}
 		}
-		
+
 	}
-	
+
 	private void handleTimeCommand(BotContext ctx, CommandMatcher matcher) {
 		String zoneName = matcher.nextWord();
-		
+
 		if(zoneName.equals("")) {
 			String saved = getUserTz(ctx, null);
 			if(saved != null) {
@@ -67,12 +69,18 @@ public class TimeHandler implements CommandHandler {
 				ctx.reply("I don't know about your timezone, honey");
 				return;
 			}
+		} else {
+			String[] knownZoneNames = TimeZone.getAvailableIDs();
+			if(!ArrayUtils.contains(knownZoneNames, zoneName)) {
+				ctx.reply("Timezone `" + zoneName + "` is unknown.");
+				return;
+			}
 		}
-		
+
 		String time = getRemoteTime(zoneName);
 		ctx.reply("It is " + time + " (" + zoneName + ")");
 	}
-	
+
 	private String getUserTz(BotContext ctx, String defaultV) {
 		String record = ctx.getRecordStr(TZ_RECORD);
 		if(record == null || record.equals("")) {
@@ -80,54 +88,54 @@ public class TimeHandler implements CommandHandler {
 		}
 		return record;
 	}
-	
+
 	private static String getRemoteTime(String zoneName) {
 		TimeZone targetTz = TimeZone.getTimeZone(zoneName);
 		TimeZone localTz = TimeZone.getDefault();
 		Calendar cal = Calendar.getInstance();
-		
+
 		// Offset to local
 		cal.setTimeZone(localTz);
 		cal.add(Calendar.MILLISECOND, localTz.getRawOffset() * -1);
 		if(localTz.inDaylightTime(cal.getTime())) {
 			cal.add(Calendar.MILLISECOND, cal.getTimeZone().getDSTSavings() * -1);
 		}
-		
+
 		// Offset to target
 		cal.add(Calendar.MILLISECOND, targetTz.getRawOffset());
 		if(targetTz.inDaylightTime(cal.getTime())) {
 			cal.add(Calendar.MILLISECOND, targetTz.getDSTSavings());
 		}
-		
+
 		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
 		String string = sdf.format(cal.getTime());
 		return string;
 	}
-	
+
 	private void handleTimeRequest(BotContext ctx, TimeRequest tr) {
 		Date utcDate = getUTCDateFromRequest(tr);
-		
+
 		List<String> converted = new ArrayList<>();
 		for(String tzName : outputZones) {
 			if(tzName.equals(tr.requestTz))
 				continue;
-			
+
 			TimeZone targetTz = TimeZone.getTimeZone(tzName);
 			Calendar targetCal = Calendar.getInstance(targetTz);
 			targetCal.setTime(utcDate);
 			int hour = targetCal.get(Calendar.HOUR_OF_DAY);
 			int minutes = targetCal.get(Calendar.MINUTE);
-			
+
 			converted.add(printTime(hour, minutes, tzName));
 		}
-		
+
 		String body = Joiner.on(", ").join(converted);
-		
+
 		String prefix = "(" + printTime(tr.hour, tr.minutes, tr.requestTz) + " = ";
 		String postfix = ")";
 		ctx.reply(prefix + body + postfix);
 	}
-	
+
 	private static Date getUTCDateFromRequest(TimeRequest tr) {
 		TimeZone userTz = TimeZone.getTimeZone(tr.requestTz);
 		Calendar userCal = Calendar.getInstance(userTz);
@@ -136,13 +144,13 @@ public class TimeHandler implements CommandHandler {
 		Date millis = userCal.getTime();
 		return millis;
 	}
-	
+
 	private static String printTime(int hour, int minutes, String tz) {
 		String[] tzParts = tz.split("/");
 		String tzLast = tzParts[tzParts.length - 1];
 		return String.format("%02d", hour) + ":" + String.format("%02d", minutes) + " in " + tzLast;
 	}
-	
+
 	private TimeRequest findTimeRequest(String userTz, String msg) {
 		Matcher m = timePattern.matcher(msg);
 		if(m.find()) {
@@ -155,29 +163,29 @@ public class TimeHandler implements CommandHandler {
 				}
 				tr.minutes = Integer.parseInt(minutesStr);
 				tr.requestTz = userTz;
-				
+
 				return tr;
 			} catch (NumberFormatException e) {
 				Logging.logException(e);
 				return null;
 			}
 		}
-		
+
 		return null;
 	}
-	
+
 	@Override
 	public String getName() {
 		return "+time";
 	}
-	
+
 	@Override
 	public void help(BotContext ctx) {
 		ctx.reply("Get timezone aware time");
 		ctx.reply("Use +time <timezone> to get the time of a particular timezone.");
 		ctx.reply("If your timezone is recorded (+record set tz <yourTimeZone>), you can just use +time.");
 	}
-	
+
 	private static class TimeRequest {
 		String requestTz;
 		int hour;
