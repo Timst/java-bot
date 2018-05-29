64b2751da395396bc6e84385ebe1ca01d2c07829
ThomasGirard <arrg.ch@gmail.com>
2018-05-29 19:14:51 +0100

    Code formatting

--- src/main/java/ch/arrg/javabot/util/CachedResource.java
+++ src/main/java/ch/arrg/javabot/util/CachedResource.java
@@ -6,38 +6,38 @@ import java.util.function.Supplier;
 public class CachedResource<T> {
 	private final Supplier<T> supplierFn;
 	private final long cacheDurationMs;
-	
+
 	private T cachedInstance;
 	private long lastUpdated;
-
-	public static <T> CachedResource<T> make(Supplier<T> supplierFn, int cacheDuration, TimeUnit cacheTimeUnit) {
-		return new CachedResource(supplierFn, cacheDuration, cacheTimeUnit);
-	}
 	
+	public static <T> CachedResource<T> make(Supplier<T> supplierFn, int cacheDuration, TimeUnit cacheTimeUnit) {
+		return new CachedResource<T>(supplierFn, cacheDuration, cacheTimeUnit);
+	}
+
 	public CachedResource(Supplier<T> supplierFn, int cacheDuration, TimeUnit cacheTimeUnit) {
 		this.supplierFn = supplierFn;
 		this.cacheDurationMs = cacheTimeUnit.toMillis(cacheDuration);
 		update();
 	}
-
+	
 	public void update() {
 		T newValue = supplierFn.get();
-
+		
 		if(newValue != null) {
 			cachedInstance = newValue;
 			lastUpdated = System.currentTimeMillis();
 		}
 	}
-
+	
 	public void invalidate() {
 		lastUpdated = 0;
 	}
-	
+
 	public T get() {
 		if(System.currentTimeMillis() > lastUpdated + cacheDurationMs) {
 			update();
 		}
-		
+
 		return cachedInstance;
 	}
 }
