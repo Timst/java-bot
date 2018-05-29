a2f3f70feadc23446810ce42df2a9dce34ab3227
ThomasGirard <arrg.ch@gmail.com>
2018-05-29 19:19:56 +0100

    MemoHandler can notify multiple users

--- src/main/java/ch/arrg/javabot/handlers/MemoHandler.java
+++ src/main/java/ch/arrg/javabot/handlers/MemoHandler.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import com.google.common.collect.Lists;
+
 import ch.arrg.javabot.IrcEventHandler;
 import ch.arrg.javabot.data.BotContext;
 import ch.arrg.javabot.data.UserData;
@@ -12,58 +14,67 @@ import ch.arrg.javabot.data.UserDb;
 import ch.arrg.javabot.util.CommandMatcher;
 
 public class MemoHandler implements IrcEventHandler {
-	
+
 	private static final String DATA_KEY = "MemoHandler";
-	
+
 	@Override
 	public void handle(BotContext ctx) {
-		
+
 		CommandMatcher matcher = CommandMatcher.make("+memo");
 		if(matcher.matches(ctx.message)) {
-			String user = matcher.nextWord();
+			List<String> users = readUsers(matcher);
+
+			for(String user : users) {
+				MemoMessage msg = new MemoMessage(ctx.sender, user, new Date(), matcher.remaining());
+				storeMessage(ctx, msg);
+			}
 			
-			MemoMessage msg = new MemoMessage(ctx.sender, user, new Date(), matcher.remaining());
-			storeMessage(ctx, msg);
-			
-			ctx.reply("Ok I'll tell " + user + " when xir get back.");
+			String feedback = users.size() == 1 ? users.get(0) : "them";
+			ctx.reply("Ok I'll tell " + feedback + " when they get back.");
 		}
+	}
+
+	private static List<String> readUsers(CommandMatcher matcher) {
+		String userList = matcher.nextWord();
+		String[] users = userList.split(";");
+		return Lists.newArrayList(users);
 	}
 	
 	private static void storeMessage(BotContext ctx, MemoMessage msg) {
 		String userCanon = UserDb.canonize(msg.to);
-		
+
 		UserData dat = ctx.getUserData(userCanon);
 		List<MemoMessage> messages = dat.getOrInit(DATA_KEY, new ArrayList<MemoMessage>());
-		
+
 		messages.add(msg);
-		
+
 		dat.setRecord(DATA_KEY, messages);
 	}
-	
+
 	@Override
 	public void onJoin(String user, BotContext ctx) {
 		String userCanon = UserDb.canonize(user);
-		
+
 		UserData dat = ctx.getUserData(userCanon);
 		List<MemoMessage> messages = dat.getOrInit(DATA_KEY, new ArrayList<MemoMessage>());
-		
+
 		if(messages != null && !messages.isEmpty()) {
 			ctx.reply("Hi " + user + ", here are messages for you : ");
 			for(MemoMessage msg : messages) {
 				ctx.reply("From " + msg.from + " on " + msg.when + " : " + msg.msg);
 			}
-			
+
 			messages.clear();
 			dat.setRecord(DATA_KEY, messages);
 		}
 	}
-	
+
 	static class MemoMessage implements Serializable {
 		private final String from;
 		private final String to;
 		private final Date when;
 		private final String msg;
-		
+
 		public MemoMessage(String from, String to, Date when, String msg) {
 			super();
 			this.from = from;
@@ -72,12 +83,12 @@ public class MemoHandler implements IrcEventHandler {
 			this.msg = msg;
 		}
 	}
-	
+
 	@Override
 	public String getName() {
 		return "+memo";
 	}
-	
+
 	@Override
 	public void help(BotContext ctx) {
 		ctx.reply("Store a message for when a user connects.");
