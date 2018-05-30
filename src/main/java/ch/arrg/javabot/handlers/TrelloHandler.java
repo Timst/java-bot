06dc2cb11fdba88cc96f10cc655a88ff9a6d567c
ThomasGirard <arrg.ch@gmail.com>
2018-05-30 20:18:03 +0100

    TrelloHandler handles more flexible creation and description of cards

--- src/main/java/ch/arrg/javabot/handlers/TrelloHandler.java
+++ src/main/java/ch/arrg/javabot/handlers/TrelloHandler.java
@@ -30,36 +30,36 @@ public class TrelloHandler implements CommandHandler {
 	private List<String> allowedLists = new ArrayList<String>();
 	// TODO Trello : make configurable newListId
 	private String newListId = "5607b4fa318dc47ac0060bbd";
-
+	
 	private CurrentTask currentTask;
-
+	
 	public TrelloHandler() {
 		String apiKey = Const.str("trello.api_key");
 		String token = Const.str("trello.api_token");
 		boardId = Const.str("trello.board_id");
-
+		
 		// TODO deprecated
-
+		
 		DefaultHttpClient httpClient = new DefaultHttpClient();
 		TrelloHttpClient thc = new ApacheHttpClient(httpClient);
 		trello = new TrelloImpl(apiKey, token, thc);
-
+		
 		allowedLists.add("5607b5067c7a5934876c228a");
 		allowedLists.add("5607b5017819b8f537470681");
 		allowedLists.add("5607b4fa318dc47ac0060bbd");
 		allowedLists.add("5607b50955a6260f4aaa4be4");
 	}
-
+	
 	@Override
 	public String getName() {
 		return "+trello";
 	}
-
+	
 	@Override
 	public void help(BotContext ctx) {
 		ctx.reply("+trello (read|link|todo|description) <gamename> <streamnumber>");
 	}
-
+	
 	@Override
 	public void handle(BotContext ctx) {
 		CommandMatcher cm = CommandMatcher.make("+trello");
@@ -73,6 +73,8 @@ public class TrelloHandler implements CommandHandler {
 				handleLink(ctx, cm);
 			} else if("create".equals(action)) {
 				handleCreate(ctx, cm);
+			} else if("describe".equals(action)) {
+				handleDescription(ctx, cm);
 			} else if("description".equals(action)) {
 				handleDescription(ctx, cm);
 			} else if("commit".equals(action)) {
@@ -81,28 +83,28 @@ public class TrelloHandler implements CommandHandler {
 				handleCancel(ctx, cm, false);
 			}
 		}
-
+		
 		if(currentTask != null) {
 			tryRecordForCurrentTask(ctx);
 		}
 	}
-
+	
 	private void tryRecordForCurrentTask(BotContext ctx) {
 		if(ctx.message.startsWith("+")) {
 			return;
 		}
-
+		
 		if(currentTask == null) {
 			return;
 		}
-
+		
 		if(!currentTask.owner.equals(ctx.sender)) {
 			return;
 		}
-
+		
 		currentTask.lines.add(ctx.message);
 	}
-
+	
 	private void handleCancel(BotContext ctx, CommandMatcher cm, boolean silentOnNone) {
 		if(currentTask != null) {
 			String name = currentTask.card.game + " " + currentTask.card.number;
@@ -112,76 +114,113 @@ public class TrelloHandler implements CommandHandler {
 			ctx.reply("No pending description to cancel.");
 		}
 	}
-
+	
 	private void handleCommit(BotContext ctx, CommandMatcher cm) {
 		if(currentTask == null) {
 			ctx.reply("No pending description to commit.");
 			return;
 		}
-
+		
 		String name = currentTask.card.game + " " + currentTask.card.number;
-
+		
 		Optional<Card> cardO = getCard(currentTask.card);
 		if(!cardO.isPresent()) {
 			ctx.reply("Card `" + name + "` not found on Trello. Create it before commiting.");
 			return;
 		}
-
+		
 		Card card = cardO.get();
 		String newDesc = Joiner.on("\n").join(currentTask.lines);
 		card.setDesc(newDesc);
 		trello.updateCard(card);
 		ctx.reply("Card `" + name + "` updated : " + card.getUrl());
-
+		
 		currentTask = null;
 	}
-
+	
 	private void handleDescription(BotContext ctx, CommandMatcher cm) {
 		if(currentTask != null) {
 			handleCancel(ctx, cm, true);
 		}
-
+		
 		cm.nextWord();
 		CardInfo ci = getCardInfoFromCommand(cm);
 		Optional<Card> oc = getCard(ci);
-
+		
 		if(!oc.isPresent()) {
 			ctx.reply("Card not found");
 			return;
 		}
 		Card card = oc.get();
+		
+		String remaining = cm.remaining();
+		if(remaining.length() > 1) {
+			// Inline mode
+			doEditCard(ctx, card, remaining);
+			
+		} else {
+			// Interactive mode
+			ctx.reply("Enter title (with \"= \") and description for " + card.getName()
+					+ ", use +trello commit or +trello cancel once finished.");
 
-		ctx.reply("Enter title (with \"= \") and description for " + card.getName()
-				+ ", use +trello commit or +trello cancel once finished.");
-
-		CurrentTask ct = new CurrentTask();
-		ct.card = ci;
-		ct.lines = new ArrayList<String>();
-		ct.owner = ctx.sender;
-
-		currentTask = ct;
+			CurrentTask ct = new CurrentTask();
+			ct.card = ci;
+			ct.lines = new ArrayList<String>();
+			ct.owner = ctx.sender;
+			
+			currentTask = ct;
+		}
 	}
-
+	
+	private void doEditCard(BotContext ctx, Card card, String desc) {
+		desc = desc.replace("\\n", "\n");
+		card.setDesc(desc);
+		trello.updateCard(card);
+		ctx.reply("Card `" + card.getName() + "` updated : " + card.getUrl());
+	}
+	
 	private void handleCreate(BotContext ctx, CommandMatcher cm) {
 		cm.nextWord();
 		String game = cm.nextWord();
 
-		String newCardNoS = findNextNumber(game);
-		String cardName = game + " " + newCardNoS;
+		// Determine card number (either provided explicitely, or read from
+		// trello)
+		String newCardNoS = null;
+		String maybeNumber = cm.nextWord();
+		if(maybeNumber.matches("\\d+")) {
+			newCardNoS = maybeNumber;
+		} else {
+			cm.popWord();
+			newCardNoS = findNextNumber(game);
+		}
 
+		// Check if card exists
+		CardInfo ci = new CardInfo(game, newCardNoS);
+		if(getCard(ci).isPresent()) {
+			ctx.reply("This card already exists. Operation cancelled.");
+			return;
+		}
+
+		// Read description
+		String desc = cm.remaining();
+		desc = desc.replace("\\n", "\n");
+
+		String cardName = game + " " + newCardNoS;
 		// Create card
 		// TODO Trello create card : insert at end of list
 		// TODO Trello create card : attach to a user
 		Card card = new Card();
 		card.setName(cardName);
 		card.setPos(999);
+		card.setDesc(desc);
+		// TODO Trello warn if card already exists
 		Card created = trello.createCard(newListId, card);
-
+		
 		// Reply
 		String url = created.getUrl();
 		ctx.reply("Card created : " + url);
 	}
-
+	
 	private String findNextNumber(String game) {
 		Stream<Card> cards = getAllCards(true);
 		Optional<Integer> maxNoO = cards
@@ -189,34 +228,34 @@ public class TrelloHandler implements CommandHandler {
 				.filter(n -> n.startsWith(game))
 				.map(n -> getNumberFromName(n))
 				.max(Integer::compare);
-
+		
 		int maxNo = maxNoO.orElse(0);
-
+		
 		int newCardNo = 1;
 		if(maxNo > 0) {
 			newCardNo = maxNo + 1;
 		}
-
+		
 		return integerToPaddedString(newCardNo);
 	}
-
+	
 	private Optional<Card> getCard(CardInfo ci) {
 		String expectedName = ci.game + " " + ci.number;
 		Stream<Card> cards = getAllCards(true);
 		cards = cards.filter(c -> c.getName().equals(expectedName));
 		return cards.findFirst();
 	}
-
+	
 	private void handleRead(BotContext ctx, CommandMatcher cm) {
 		cm.nextWord();
 		CardInfo ci = getCardInfoFromCommand(cm);
 		Optional<Card> oc = getCard(ci);
-
+		
 		if(!oc.isPresent()) {
 			ctx.reply("Card not found");
 			return;
 		}
-
+		
 		Card c = oc.get();
 		ctx.reply("Found card `" + c.getName() + "`, description : ");
 		String desc = c.getDesc();
@@ -225,59 +264,57 @@ public class TrelloHandler implements CommandHandler {
 			ctx.reply("| " + line);
 		}
 	}
-
+	
 	private void handleLink(BotContext ctx, CommandMatcher cm) {
 		cm.nextWord();
 		CardInfo ci = getCardInfoFromCommand(cm);
 		Optional<Card> oc = getCard(ci);
-
+		
 		if(!oc.isPresent()) {
 			ctx.reply("Card not found");
 			return;
 		}
-
+		
 		Card c = oc.get();
 		ctx.reply("Found card `" + c.getName() + "`: " + c.getUrl());
 	}
-
+	
 	private void handleMissing(BotContext ctx, CommandMatcher cm) {
 		Stream<Card> cards = getAllCards(false);
-
+		
 		// Group by game
 		Map<String, List<CardInfo>> todo = cards
 				.filter(c -> allowedLists.contains(c.getIdList()))
 				.filter(c -> "".equals(c.getDesc()))
 				.map(c -> infoFromCard(c))
 				.collect(Collectors.groupingBy(c -> c.game));
-
+		
 		if(todo.isEmpty()) {
 			ctx.reply("No cards to do");
 		} else {
 			int count = todo.values().stream().map(l -> l.size()).mapToInt(Integer::intValue).sum();
 			ctx.reply(count + " cards are missing descriptions: ");
-
+			
 			// Sort sublists and display
 			todo.forEach((g, l) -> ctx.reply(g + ": " + Joiner.on(", ").join(
 					l.stream().map(ci -> ci.number).sorted().iterator())));
 		}
 	}
-
+	
 	private Stream<Card> getAllCards(boolean includeArchived) {
 		Argument filter = new Argument("filter", includeArchived ? "all" : "open");
 		Stream<Card> allCards = trello.getBoardCards(boardId, filter).stream();
 		return allCards;
 	}
-
+	
 	private static CardInfo getCardInfoFromCommand(CommandMatcher cm) {
-		CardInfo card = new CardInfo();
-		card.game = cm.nextWord();
-		card.number = cm.nextWord();
+		CardInfo card = new CardInfo(cm.nextWord(), cm.nextWord());
 		return card;
 	}
-
+	
 	private static CardInfo infoFromCard(Card c) {
 		String cardName = c.getName();
-
+		
 		// Split title "gamename 01"
 		int space = cardName.indexOf(' ');
 		String game;
@@ -289,24 +326,28 @@ public class TrelloHandler implements CommandHandler {
 			game = cardName.substring(0, space);
 			number = cardName.substring(space + 1);
 		}
-
-		CardInfo ci = new CardInfo();
-		ci.game = game;
-		ci.number = number;
+		
+		CardInfo ci = new CardInfo(game, number);
 		return ci;
 	}
-
+	
 	private static class CardInfo {
+		public CardInfo(String game, String number) {
+			super();
+			this.game = game;
+			this.number = number;
+		}
+		
 		String game;
 		String number;
 	}
-
+	
 	private class CurrentTask {
 		CardInfo card;
 		String owner;
 		List<String> lines;
 	}
-
+	
 	/** pubg 02 -> 2 */
 	private static int getNumberFromName(String name) {
 		int space = name.indexOf(' ');
@@ -320,7 +361,7 @@ public class TrelloHandler implements CommandHandler {
 			return -1;
 		}
 	}
-
+	
 	/** 1 -> "01"
 	 * 10 -> "10" */
 	private static String integerToPaddedString(int newCardNo) {
