c40f9894832eb5327011b783e3a590b5d40f9657
ThomasGirard <arrg.ch@gmail.com>
2018-05-30 20:17:48 +0100

    CommandMatcher can now pop a word back on the stack

--- src/main/java/ch/arrg/javabot/util/CommandMatcher.java
+++ src/main/java/ch/arrg/javabot/util/CommandMatcher.java
@@ -6,15 +6,15 @@ import java.util.regex.Pattern;
 import com.google.common.base.Joiner;
 
 public class CommandMatcher {
-	
+
 	private final String pattern;
 	private String[] split;
 	private int splitIdx;
-	
+
 	private CommandMatcher(String pattern) {
 		this.pattern = pattern;
 	}
-	
+
 	/** Returns true iff the line matches the pattern, if true, consumes the
 	 * pattern. */
 	public boolean matches(String line) {
@@ -24,37 +24,44 @@ public class CommandMatcher {
 			split = remaining.split("\\s+");
 			splitIdx = 0;
 		}
-		
+
 		return matches;
 	}
-	
+
 	public static CommandMatcher make(String... items) {
 		String line = Joiner.on(" ").join(items);
 		return new CommandMatcher(line);
 	}
-	
+
 	public String nextWord() {
 		if(splitIdx < split.length) {
 			String word = split[splitIdx];
 			splitIdx++;
 			return word;
 		}
-		
+
 		return "";
 	}
 	
+	/** Moves the pointer back 1 word (same as if the previous call to nextWord
+	 * hadn't happened. */
+	public void popWord() {
+		if(splitIdx >= 1)
+			splitIdx--;
+	}
+
 	public String remaining() {
 		String[] remainingWords = Arrays.copyOfRange(split, splitIdx, split.length);
 		return Joiner.on(' ').join(remainingWords);
 	}
-
+	
 	public String peekWord() {
 		if(splitIdx < split.length) {
 			String word = split[splitIdx];
 			return word;
 		}
-		
+
 		return "";
 	}
-	
+
 }
