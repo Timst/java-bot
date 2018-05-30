dd82be6cb90c66b5aaa6ac36c38d7699a89cd499
ThomasGirard <arrg.ch@gmail.com>
2018-05-30 20:46:32 +0100

    Add flag letters mode to +text

--- src/main/java/ch/arrg/javabot/handlers/TextConversionHandler.java
+++ src/main/java/ch/arrg/javabot/handlers/TextConversionHandler.java
@@ -9,22 +9,23 @@ import ch.arrg.javabot.util.CommandMatcher;
 
 public class TextConversionHandler implements CommandHandler {
 	public static enum Mode {
-		FULLWIDTH(TextConversionHandler::toFullwidth);
-
+		FULLWIDTH(TextConversionHandler::toFullwidth),
+		FLAGLETTERS(TextConversionHandler::toFlagLetters);
+		
 		private Function<String, String> convFun;
-
+		
 		private Mode(Function<String, String> convFun) {
 			this.convFun = convFun;
 		}
-
+		
 		public String convert(String text) {
 			return convFun.apply(text);
 		}
 	};
-
+	
 	@Override
 	public void handle(BotContext ctx) {
-
+		
 		CommandMatcher matcher = CommandMatcher.make("+text");
 		if(matcher.matches(ctx.message)) {
 			Mode mode = getMode(matcher.nextWord());
@@ -37,7 +38,7 @@ public class TextConversionHandler implements CommandHandler {
 			}
 		}
 	}
-
+	
 	private Mode getMode(String nextWord) {
 		try {
 			return Mode.valueOf(nextWord.toUpperCase());
@@ -45,23 +46,24 @@ public class TextConversionHandler implements CommandHandler {
 			return null;
 		}
 	}
-
+	
 	@Override
 	public String getName() {
 		return "+text";
 	}
-
+	
 	@Override
 	public void help(BotContext ctx) {
-		ctx.reply("Convert text to different formats : +text <format> <text>. Supported formats : fullwidth.");
+		ctx.reply("Convert text to different formats : +text <format> <text>.");
+		ctx.reply("Supported formats: fullwidth, flagletters.");
 	}
-
+	
 	private static String toFullwidth(String text) {
 		char firstFull = '！';
 		// char lastFull = 'ｚ';
 		char firstIn = '!';
 		char lastIn = 'z';
-
+		
 		// TODO performance is bad
 		char out = firstFull;
 		for(char in = firstIn; in <= lastIn; in++) {
@@ -69,8 +71,33 @@ public class TextConversionHandler implements CommandHandler {
 			out++;
 		}
 		text = text.replaceAll(" ", "   ");
-
+		
 		return text;
 	}
-
+	
+	private static String toFlagLetters(String text) {
+		String flagA = "🇦";
+		
+		// TODO performance is bad
+		StringBuilder sb = new StringBuilder();
+		char[] chars = text.toCharArray();
+		for(int i = 0; i < chars.length; i++) {
+			char c = chars[i];
+			int d = -1;
+			if(c >= 'a' && c <= 'z') {
+				d = c - 'a';
+			} else if(c >= 'A' && c <= 'Z') {
+				d = c - 'A';
+			}
+			
+			if(d >= 0) {
+				sb = sb.append(flagA.charAt(0)).append((char) (flagA.charAt(1) + d));
+			} else {
+				sb = sb.append(c);
+			}
+		}
+		
+		return sb.toString();
+	}
+	
 }
